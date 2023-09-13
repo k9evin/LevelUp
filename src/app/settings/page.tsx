ca99b81e867d143d6ce1fa0eeb24185d603a0a45
Mingkai Pang <pangmingkai@gmail.com>
2023-09-13 02:04:51 -0700

    update settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -8,8 +8,8 @@ const SettingsPage = async (props: Props) => {
   const isPremium = await checkSubscription();
   return (
     <div className="p-8 mx-auto max-w-7xl">
-      <h1 className="text-3xl font-bold mb-4">Settings</h1>
-      {isPremium ? <p>You are a premium user</p> : <p>You are a free user</p>}
+      <h1 className="text-3xl font-bold mb-4">Subscription</h1>
+      {isPremium ? <p>You are a premium user.</p> : <p>You are a free user.</p>}
 
       <SubscriptionButton isPremium={isPremium} />
     </div>
